6908a08861da6ba96a87057d89d06fd284ab42cc
Muhammad Hewedy <mhewedy@gmail.com>
2018-04-14 10:18:22 +0300

    init

---
+++ src/main/java/primitivebank/PrimitiveBankApplication.java
@@ -0,0 +1,12 @@
+package primitivebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimitiveBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimitiveBankApplication.class, args);
+	}
+}
---
+++ src/test/java/primitivebank/PrimitiveBankApplicationTests.java
@@ -0,0 +1,16 @@
+package primitivebank;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PrimitiveBankApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
