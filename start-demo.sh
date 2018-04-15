d6a1fe1fc132b0b99029eff8a5547269bb88714a
Muhammad Hewedy <mhewedy@gmail.com>
2018-04-15 08:52:34 +0300

    fine tune the run script (start-demo.sh)

--- start-demo.sh
+++ start-demo.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-mvn clean package -DskipTests
+# to kill all sub processes when this script killed
+trap 'kill -HUP 0' EXIT
+
+if [ "$1" != "-skip--build" ]
+then
+    mvn clean package -DskipTests
+fi
 
 java $JAVA_OPTS -jar target/primitive-bank-*.jar --spring.profiles.active=demo &
 
